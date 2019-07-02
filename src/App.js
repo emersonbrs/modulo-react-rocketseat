13f89a3ce395ccae8994219dab733d170b453391
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 22:23:19 -0300

    Criando componente raiz

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react"; // Todo lugar que usar a sintaxe JSX, o React precisa estar sendo importado
+
+function App() {
+  return <h1>Hello Rocketseat</h1>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-const soma = (a, b) => a + b;
+import React from "react";
+import { render } from "react-dom";
 
-alert(soma(2, 3));
+import App from "./App";
+
+// O code dentro do render abaixo procura um elemento, dentro do meu document, com o id app e adiciona o componente App
+render(<App />, document.getElementById("app"));
