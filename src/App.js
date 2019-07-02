01375a1b34c0d15a7e4b6c46fbc9eb2bbe399166
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 22:37:41 -0300

    Importando Imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"; // Todo lugar que usar a sintaxe JSX, o React precisa estar sendo importado
 import "./App.css";
 
+import lua from "./assets/lua.jpg";
+
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="200" src={lua} />;
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i, // O i significa que é case insensitive
+        use: {
+          loader: "file-loader"
+        }
       }
     ]
   }
