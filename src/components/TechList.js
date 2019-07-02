3ab21277d0c229a39669073a32593ca59164c809
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-02 00:19:45 -0300

    Adicionando ciclo de vida do componente

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -16,8 +16,28 @@ class TechList extends Component {
   // é preciso instalar a dependência @babel/plugin-proposal-class-properties -D
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props(propriedades do componente) ou estado
+  // componentDidUpdate(prevProps, prevState) {}
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = event => {
     this.setState({ newTech: event.target.value });
