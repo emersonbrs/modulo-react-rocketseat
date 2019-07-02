22f1e8624007e48f58579a51e0df2a9f931f9334
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 23:57:04 -0300

    Trabalhando com propriedades no React

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TechItem from "./TechItem";
 
 class TechList extends Component {
   /*constructor() {
@@ -37,12 +38,11 @@ class TechList extends Component {
         <form onSubmit={this.handleSubmit}>
           <ul>
             {this.state.techs.map(tech => (
-              <li key={tech}>
-                {tech}
-                <button onClick={() => this.handleDelete(tech)} type="button">
-                  Remover
-                </button>
-              </li>
+              <TechItem
+                key={tech}
+                tech={tech}
+                onDelete={() => this.handleDelete(tech)}
+              />
             ))}
           </ul>
           <input
