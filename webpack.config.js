d98e3f7d9a7121392e92cdd168c6e7fe52afa79b
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 21:47:42 -0300

    Criando componente raiz

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    // output direciona ao local que vai ser jogado o Bundle (código transpilado pelo webpack)
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    // devServer direciona ao local que vai encontrar o nosso index.html
+    contentBase: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Expressão Regular usa as barras
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
